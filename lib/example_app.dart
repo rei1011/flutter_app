d6efa5b337941852e81ba69750a03e422988cccd
rei1011 <r.mizobuchi3@gmail.com>
2022-11-27 19:38:41 +0900

    dynamic linksのexampleを追加

--- lib/example_app.dart
+++ lib/example_app.dart
@@ -1,22 +1,34 @@
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app/dynamic_link_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'constants.dart';
 import 'hero.dart';
 import 'user.dart';
 
-class ExampleApp extends StatefulWidget {
+class MainScreen extends StatefulWidget {
   final Auth0? auth0;
-  const ExampleApp({this.auth0, final Key? key}) : super(key: key);
+  const MainScreen({this.auth0, final Key? key}) : super(key: key);
 
   @override
-  State<ExampleApp> createState() => _ExampleAppState();
+  State<MainScreen> createState() => MainScreenState();
 }
 
-class _ExampleAppState extends State<ExampleApp> {
+class MainScreenState extends State<MainScreen> {
+  String? _linkMessage;
+  bool _isCreatingLink = false;
+  FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
+  final String _testString =
+      'To test: long press link and then copy and click from a non-browser '
+      "app. Make sure this isn't being tested on iOS simulator and iOS xcode "
+      'is properly setup. Look at firebase_dynamic_links/README.md for more '
+      'details.';
+  final String DynamicLink = 'https://example/helloworld';
+  final String Link = 'https://flutterappdev.page.link/oN6u';
   UserProfile? _user;
   late Auth0 auth0;
 
@@ -39,6 +51,42 @@ class _ExampleAppState extends State<ExampleApp> {
     }).onError((error) {
       print('onLink error');
       print(error.message);
+    });
+  }
+
+  Future<void> _createDynamicLink(bool short) async {
+    setState(() {
+      _isCreatingLink = true;
+    });
+
+    final DynamicLinkParameters parameters = DynamicLinkParameters(
+      uriPrefix: 'https://flutterappdev.page.link',
+      longDynamicLink: Uri.parse(
+        'https://flutterappdev.page.link?efr=0&ibi=io.flutter.plugins.firebase.dynamiclinksexample&apn=io.flutter.plugins.firebase.dynamiclinksexample&imv=0&amv=0&link=https%3A%2F%2Fexample%2Fhelloworld&ofl=https://ofl-example.com',
+      ),
+      link: Uri.parse(DynamicLink),
+      androidParameters: const AndroidParameters(
+        packageName: 'io.flutter.plugins.firebase.dynamiclinksexample',
+        minimumVersion: 0,
+      ),
+      iosParameters: const IOSParameters(
+        bundleId: 'io.flutter.plugins.firebase.dynamiclinksexample',
+        minimumVersion: '0',
+      ),
+    );
+
+    Uri url;
+    if (short) {
+      final ShortDynamicLink shortLink =
+          await dynamicLinks.buildShortLink(parameters);
+      url = shortLink.shortUrl;
+    } else {
+      url = await dynamicLinks.buildLink(parameters);
+    }
+
+    setState(() {
+      _linkMessage = url.toString();
+      _isCreatingLink = false;
     });
   }
 
@@ -104,7 +152,68 @@ class _ExampleAppState extends State<ExampleApp> {
                             MaterialStateProperty.all<Color>(Colors.black),
                       ),
                       child: const Text('Login'),
-                    )
+                    ),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () async {
+                      final PendingDynamicLinkData? data =
+                          await dynamicLinks.getInitialLink();
+                      final Uri? deepLink = data?.link;
+
+                      if (deepLink != null) {
+                        // ignore: unawaited_futures
+                        Navigator.pushNamed(context, deepLink.path);
+                      }
+                    },
+                    child: const Text('getInitialLink'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final PendingDynamicLinkData? data =
+                          await dynamicLinks.getDynamicLink(Uri.parse(Link));
+                      final Uri? deepLink = data?.link;
+
+                      if (deepLink != null) {
+                        // ignore: unawaited_futures
+                        Navigator.pushNamed(context, deepLink.path);
+                      }
+                    },
+                    child: const Text('getDynamicLink'),
+                  ),
+                  ElevatedButton(
+                    onPressed: !_isCreatingLink
+                        ? () => _createDynamicLink(false)
+                        : null,
+                    child: const Text('Get Long Link'),
+                  ),
+                  ElevatedButton(
+                    onPressed: !_isCreatingLink
+                        ? () => _createDynamicLink(true)
+                        : null,
+                    child: const Text('Get Short Link'),
+                  ),
+                ],
+              ),
+              InkWell(
+                onTap: () async {
+                  if (_linkMessage != null) {
+                    await launchUrl(Uri.parse(_linkMessage!));
+                  }
+                },
+                onLongPress: () {
+                  Clipboard.setData(ClipboardData(text: _linkMessage));
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Copied Link!')),
+                  );
+                },
+                child: Text(
+                  _linkMessage ?? '',
+                  style: const TextStyle(color: Colors.blue),
+                ),
+              ),
+              Text(_linkMessage == null ? '' : _testString)
             ],
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/dynamic_link_page.dart';
 import 'package:flutter_app/firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'example_app.dart';
@@ -9,5 +10,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const ExampleApp());
+  runApp(
+    MaterialApp(
+      title: 'Dynamic Links Example',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => const MainScreen(),
+        '/helloworld': (BuildContext context) => const DynamicLinkPage(),
+      },
+    ),
+  );
 }
