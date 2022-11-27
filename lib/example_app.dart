110616fc95a48b377b6d04888963c0e3e99f332c
rei1011 <r.mizobuchi3@gmail.com>
2022-11-27 16:52:15 +0900

    Dynamic Linksの導入

--- lib/example_app.dart
+++ lib/example_app.dart
@@ -1,6 +1,8 @@
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/dynamic_link_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 
 import 'constants.dart';
 import 'hero.dart';
@@ -16,14 +18,25 @@ class ExampleApp extends StatefulWidget {
 
 class _ExampleAppState extends State<ExampleApp> {
   UserProfile? _user;
-
   late Auth0 auth0;
+  FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
 
   @override
   void initState() {
     super.initState();
+    initDynamicLinks();
     auth0 = widget.auth0 ??
         Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
+  }
+
+  Future<void> initDynamicLinks() async {
+    dynamicLinks.onLink.listen((dynamicLinkData) {
+      debugPrint('dynamicLinkData.link.path = ${dynamicLinkData.link.path}');
+      Navigator.pushNamed(context, dynamicLinkData.link.path);
+    }).onError((error) {
+      print('onLink error');
+      print(error.message);
+    });
   }
 
   Future<void> login() async {
@@ -50,39 +63,49 @@ class _ExampleAppState extends State<ExampleApp> {
   Widget build(final BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Padding(
-        padding: const EdgeInsets.only(
-          top: padding,
-          bottom: padding,
-          left: padding / 2,
-          right: padding / 2,
+        body: Padding(
+          padding: const EdgeInsets.only(
+            top: padding,
+            bottom: padding,
+            left: padding / 2,
+            right: padding / 2,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    _user != null
+                        ? UserWidget(user: _user)
+                        : const Expanded(child: HeroWidget())
+                  ],
+                ),
+              ),
+              _user != null
+                  ? ElevatedButton(
+                      onPressed: logout,
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black),
+                      ),
+                      child: const Text('Logout'),
+                    )
+                  : ElevatedButton(
+                      onPressed: login,
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black),
+                      ),
+                      child: const Text('Login'),
+                    )
+            ],
+          ),
         ),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          Expanded(
-              child: Column(children: [
-            _user != null
-                ? UserWidget(user: _user)
-                : const Expanded(child: HeroWidget())
-          ])),
-          _user != null
-              ? ElevatedButton(
-                  onPressed: logout,
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
-                  ),
-                  child: const Text('Logout'),
-                )
-              : ElevatedButton(
-                  onPressed: login,
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
-                  ),
-                  child: const Text('Login'),
-                )
-        ]),
-      )),
+      ),
+      routes: <String, WidgetBuilder>{
+        '/helloworld': (BuildContext context) => const DynamicLinkPage(),
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'example_app.dart';
 
 void main() async {
-
-   await dotenv.load();
+  await dotenv.load();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(const ExampleApp());
 }
-
